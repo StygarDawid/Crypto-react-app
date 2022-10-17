d457fd0680ad4814e5a8805ea40d2cf1adc71b1d
twój login do GitHuba <stygar.d11@gmail.com>
2022-10-17 23:27:28 +0200

    Coin.js

--- src/routes/Coin.js
+++ src/routes/Coin.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import {useParams} from "react-router-dom";
+import axios from "axios";
+import './Coin.scss'
 
 const Coin = () => {
+    const params = useParams();
+    const [coin, setCoin] = useState({});
+
+    const url =`https://api.coingecko.com/api/v3/coins/${params.coinId}`
+
+    useEffect(() =>{
+        axios.get(url).then((res) => {
+            setCoin(res.data)
+        }).catch((error) => {
+            console.log(error)
+        })
+    }, [])
     return (
         <div>
-            <h1></h1>
+            <h1>{coin.id}</h1>
         </div>
     )
 }
